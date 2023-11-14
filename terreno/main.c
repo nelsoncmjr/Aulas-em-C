67a29eb83b696a7937a4e62c906c6989900c6b1e
Nelson Melo <99028863+patamar4@users.noreply.github.com>
2023-11-14 13:27:24 -0300

    final

--- terreno/main.c
+++ terreno/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    double largura, comprimento, valorMetro, preco, area;
+
+    printf("Digite a largura do terreno: ");
+    scanf("%lf", &largura);
+
+    printf("Digite o comprimento do terreno: ");
+    scanf("%lf", &comprimento);
+
+    printf("Digite o valor do metro quadrado: ");
+    scanf("%lf", &valorMetro);
+
+    area = largura * comprimento;
+    preco = area * valorMetro;
+
+    printf("Area do terreno = %.2lf\n", area);
+    printf("Preco do terreno = %.2lf\n", preco);
+
     return 0;
 }
